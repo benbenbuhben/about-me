1e5ca1c8fe176d9f5ab46f89d0b1b1219a57914c
Ben Hurst <BHurst8@gmail.com>
2018-03-07 20:30:06 -0800

    Files for Lab #2

--- js/app.js
+++ js/app.js
@@ -1,121 +1,175 @@
-
 "use strict";
 
-//Activate quiz when image is clicked
-document.getElementById('myFace').onclick = function() {
-  alert('Let\'s get to know Ben...');
- 
-  var numberRight=0;
 
-  //Question #1
-  var ageGuess = prompt('Do you think I\'m older than 30?');
-           
-  while (ageGuess.length < 1) {
-    alert('You did not answer!');
-    ageGuess = prompt('Do you think I\'m older than 30?');
+alert('Let\'s get to know Ben...');
+
+var numberRight = 0;
+
+//Question #1
+var ageGuess = prompt('Do you think I\'m older than 30?');
+
+while (ageGuess.length < 1) {
+  alert('You did not answer!');
+  ageGuess = prompt('Do you think I\'m older than 30?');
+}
+if (ageGuess.charAt(0).toLowerCase() === 'y') {
+  alert('Correctomundo!');
+  numberRight++;
+} else {
+  alert('Wrong!');
+}
+
+console.log('Q: Do you think I\'m older than 30?, A: ' + ageGuess);
+
+
+//Question #2
+var dogGuess = parseInt(prompt('How many dogs do you think I have?'));
+
+while (dogGuess.length < 1) {
+  alert('You did not answer!');
+  dogGuess = prompt('How many dogs do you think I have?');
+}
+
+if (dogGuess === 1) {
+  alert('Good Job!');
+  numberRight++;
+} else if (dogGuess === 0) {
+  alert('Wrong! I have one dog');
+} else if (dogGuess > 1) {
+  alert('Wrong! I have one dog!');
+} else {
+  alert('Please enter a number 0 or greater.');
+}
+
+console.log('Q: How many dogs do you think I have?, A: ' + dogGuess);
+
+//Question #3
+var favMovie = prompt('Do you think my favorite movie is Bill & Ted\'s Excellent Adventure?');
+
+while (favMovie.length < 1) {
+  alert('You did not answer!');
+  favMovie = prompt('Do you think my favorite movie is Bill & Ted\'s Excellent Adventure?');
+}
+
+if (favMovie.charAt(0).toLowerCase() === 'n') {
+  alert('How\'d you know? My favorite movie is actually Bill & Ted\'s Bogus Journey!');
+  numberRight++;
+} else {
+  alert('Wrong! My favorite movie is actually Bill & Ted\'s Bogus Journey!');
+}
+
+console.log('Q: Do you think my favorite movie is Bill & Ted\'s Excellent Adventure?, A: ' + favMovie);
+
+//Question #4
+
+var humanStatus = prompt('Am I human or droid?');
+
+while (humanStatus.length < 1) {
+  alert('You did not answer!');
+  humanStatus = prompt('Am I human or droid?');
+}
+
+if (humanStatus.toLowerCase() === 'human') {
+  alert('Yes, I am human. I like doing human things like writing code.');
+  numberRight++;
+} else {
+  alert('No. I am human. I like doing human things like writing code.');
+}
+
+console.log('Q: Am I human or droid?, A: ' + humanStatus);
+
+//Question #5
+var labQuality = prompt('Do you think this is the best lab project you\'ve ever laid your unworthy eyes upon?');
+
+while (labQuality.length < 1) {
+  alert('You did not answer!');
+  labQuality = prompt('Do you think this is the best lab project you\'ve ever laid your unworthy eyes upon?');
+}
+
+if (labQuality.charAt(0).toLowerCase() === 'y') {
+  alert('Boo Yah.');
+  numberRight++;
+} else {
+  alert('Blasphemy!');
+}
+console.log('Q: Do you think this is the best lab project you\'ve ever laid your unworthy eyes upon?, A: ' + labQuality);
+
+//Question #6
+
+var randNum = Math.floor(Math.random() * 10 + 1);
+var answer1 = false;
+var guessCount1 = 0;
+
+while (answer1 === false) {
+
+  var numGuess = prompt('Guess what number I\'m thinking of (between 1 and 10)');
+  var guess2num = parseInt(numGuess);
+
+
+  if (guess2num === randNum) {
+    alert('Woohoo!');
+    answer1 = true;
+    if (guessCount1 === 0) {
+      numberRight++;
+    }
+  } else if (guess2num > randNum) {
+    alert('Too high! Try again.');
+  } else if (guess2num < randNum) {
+    alert('Too low! Try again.');
+  } else {
+    break;
   }
-  if (ageGuess.charAt(0).toLowerCase() === 'y') {
-    alert('Correctomundo!');
-    numberRight++;
-  }
-  else {
-    alert('Wrong!');
-  }
+  guessCount1++;
+}
 
-  console.log('Q: Do you think I\'m older than 30?, A: ' + ageGuess);
- 
+//Question #7
+var answerBank = ['Mexico', 'Canada', 'Belize', 'Guatemala', 'El Salvador', 'Honduras', 'Nicaragua', 'Costa Rica', 'Panama', 'Colombia', 'Ecuador', 'Peru', 'Bolivia', 'Argentina', 'Chile', 'Uruguay'];
 
-  //Question #2
-  var dogGuess = parseInt(prompt('How many dogs do you think I have?'));
-  
-  while (dogGuess.length < 1) {
-    alert('You did not answer!');
-    dogGuess = prompt('How many dogs do you think I have?');
-  }
-       
-  if (dogGuess===1) {
-    alert('Good Job!');
-    numberRight++;
+var answer = false;
+var guessCount = 0;
+//console.log(answerBank);
+//
+
+
+while (answer === false) {
+
+  if (guessCount === 0) {
+    var countryGuess = prompt('Can you guess a country I\'ve traveled to (mostly Latin America)?');
+    console.log(countryGuess);
+  } else {
+    countryGuess = prompt('Try again! Can you guess a country I\'ve traveled to?');
+    console.log(countryGuess);
   }
 
-  else if(dogGuess===0){
-    alert('Wrong! Too low!');
-  } 
-  
-  else if(dogGuess>1){
-    alert('Wrong! Too high!');
+  for (var i = 0; i < answerBank.length; i++) {
+
+    if (countryGuess === answerBank[i]) {
+      alert('Correct!');
+      if(guessCount===0){
+        numberRight++;
+      }
+      answer = true;
+    }
   }
+  guessCount++;
 
-  else {
-    alert('Please enter a number 0 or greater.')
+  if(guessCount>6){
+    break;
   }
- 
-  console.log('Q: How many dogs do you think I have?, A: ' + dogGuess);
+}
 
-  //Question #3
-  var favMovie = prompt('Do you think my favorite movie is Bill & Ted\'s Excellent Adventure?');
- 
-  while (favMovie.length < 1) {
-    alert('You did not answer!');
-    favMovie = prompt('Do you think my favorite movie is Bill & Ted\'s Excellent Adventure?');
-  }
-       
-  if (favMovie.charAt(0).toLowerCase() === 'n') {
-    alert('How\'d you know? My favorite movie is actually Bill & Ted\'s Bogus Journey!');
-    numberRight++;
-  } 
+alert('I have been to these countries: ' + answerBank);
 
-  else {
-    alert('Wrong! My favorite movie is actually Bill & Ted\'s Bogus Journey!');
-  }
- 
-  console.log('Q: Do you think my favorite movie is Bill & Ted\'s Excellent Adventure?, A: ' + favMovie);
 
-  //Question #4
 
-  var humanStatus = prompt('Am I human or droid?');
 
-  while (humanStatus.length < 1) {
-    alert('You did not answer!');
-    humanStatus = prompt('Am I human or droid?');
-  }
-       
-  if (humanStatus.toLowerCase() === 'human') {
-    alert('Yes, I am human. I like doing human things like writing code.');
-    numberRight++;
-  } 
 
-  else {
-    alert('No. I am human. I like doing human things like writing code.');
-  }
- 
-  console.log('Q: Am I human or droid?, A: ' + humanStatus);
 
-  //Question #5
-  var labQuality = prompt('Do you think this is the best lab project you\'ve ever laid your unworthy eyes upon?');
-           
-  while (labQuality.length < 1) {
-    alert('You did not answer!');
-    labQuality = prompt('Do you think this is the best lab project you\'ve ever laid your unworthy eyes upon?');
-  }
-       
-  if (labQuality.charAt(0).toLowerCase() === 'y') {
-    alert('Boo Yah.');
-    numberRight++;
-  } 
+alert('You got ' + numberRight + ' out of 7 correct (on the first try)!');
 
-  else {
-    alert('Blasphemy!');
-  }
+//Mouse hover functionality (FCNS NOT ALLOWED IN THIS LAB)
 
-  console.log('Q: Do you think this is the best lab project you\'ve ever laid your unworthy eyes upon?, A: ' + labQuality);
- 
-  alert('You got ' + numberRight + ' out of 5 correct!'); 
-};
-
-//Mouse hover functionality
-
-document.getElementById('myFace').addEventListener('mouseover', mouseOver);
+/* document.getElementById('myFace').addEventListener('mouseover', mouseOver);
 document.getElementById('myFace').addEventListener('mouseout', mouseOut);
 
 function mouseOver() {
@@ -124,4 +178,4 @@ function mouseOver() {
 
 function mouseOut() {
   document.getElementById('myFace').style.color = 'black';
-}
+} */
